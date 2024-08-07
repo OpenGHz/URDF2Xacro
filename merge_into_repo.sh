c589a7cf94f0f528d6ed0090ecd5d90de8e9929f
Haizhou Ge <1352674740@qq.com>
2024-08-07 23:02:18 +0800

    fix cp error

--- merge_into_repo.sh
+++ merge_into_repo.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 # input obsolute paths to the raw directory and the output directory
-raw_dir=$1
-out_dir=$2
+raw_path=$1
+out_path=$2
 # get the package name from the raw directory
-package_name=$(basename $raw_dir)
+package_name=$(basename $raw_path)
 
-echo "Merging $package_name into $out_dir"
+echo "Merging $package_name into $out_path"
 
-# Merge all the files in the raw directory into out_dir
+# Merge all the files in the raw directory into out_path
 #TODO: Seperate raw meshes into visual and collision
-cp ${raw_dir}/meshes ${out_dir}/meshes/${package_name}/collision
-cp ${raw_dir}/meshes ${out_dir}/meshes/${package_name}/visual
+raw_meshes_dir=${raw_path}/meshes
+target_meshes_dir=${out_path}/meshes/${package_name}
+mkdir -p ${target_meshes_dir}/collision
+mkdir -p ${target_meshes_dir}/visual
+cp ${raw_meshes_dir}/* ${target_meshes_dir}/collision
+cp ${raw_meshes_dir}/* ${target_meshes_dir}/visual
 
 # move all the xacro files into the target urdf directory
-cp ${raw_dir}/urdf/*.xacro ${out_dir}/urdf/robots/
+cp ${raw_path}/urdf/*.xacro ${out_path}/urdf/robots/
