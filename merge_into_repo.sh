59a3abd88bfd096b00e269e3ced07930ce24824a
Haizhou Ge <1352674740@qq.com>
2024-08-08 21:15:55 +0800

    update

--- merge_into_repo.sh
+++ merge_into_repo.sh
@@ -6,8 +6,6 @@ out_path=$2
 # get the package name from the raw directory
 package_name=$(basename $raw_path)
 
-echo "Merging $package_name into $out_path"
-
 # Merge all the files in the raw directory into out_path
 #TODO: Seperate raw meshes into visual and collision
 raw_meshes_dir=${raw_path}/meshes
@@ -19,3 +17,5 @@ cp ${raw_meshes_dir}/* ${target_meshes_dir}/visual
 
 # move all the xacro files into the target urdf directory
 cp ${raw_path}/urdf/*.xacro ${out_path}/urdf/robots/
+
+echo "Merged $package_name into $out_path"
