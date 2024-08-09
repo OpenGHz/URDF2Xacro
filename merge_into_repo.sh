ee1de3b23da6e2ce482fe619d1315ba8f19566a6
Haizhou Ge <1352674740@qq.com>
2024-08-09 13:26:37 +0800

    update

--- merge_into_repo.sh
+++ merge_into_repo.sh
@@ -10,12 +10,15 @@ package_name=$(basename $raw_path)
 
 # Merge all the files in the raw directory into out_path
 #TODO: Seperate raw meshes into visual and collision
-raw_meshes_dir=${raw_path}/meshes
+raw_visual_meshes_dir=${raw_path}/meshes
+raw_collision_meshes_dir=${raw_path}/meshes
+
 target_meshes_dir=${out_path}/meshes/${package_name}
 mkdir -p ${target_meshes_dir}/collision
 mkdir -p ${target_meshes_dir}/visual
-cp ${raw_meshes_dir}/* ${target_meshes_dir}/collision
-cp ${raw_meshes_dir}/* ${target_meshes_dir}/visual
+
+cp ${raw_collision_meshes_dir}/* ${target_meshes_dir}/collision
+cp ${raw_collision_meshes_dir}/* ${target_meshes_dir}/visual
 
 # move all the xacro files into the target urdf directory
 cp ${raw_path}/urdf/*.xacro ${out_path}/urdf/robots/
